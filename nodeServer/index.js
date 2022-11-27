fa04221d10c6ee9b010cf9c5868eca250704b772
Mrityunjay Kumar <mk10399@gmail.com>
2022-11-27 21:52:48 +0530

    Chat Application
    Chat Application

---
+++ nodeServer/index.js
@@ -0,0 +1,23 @@
+// node server which will handle socket io connections
+const io = require("socket.io")(8000);
+const users = {};
+
+ io.on('connection', socket=>{
+    // If any new user joins, let other users connected to the server know
+     socket.on('new-user-joined', name=>{
+         console.log("New user", name);
+         users[socket.id] = name;
+         socket.broadcast.emit('user-joined', name);
+     });
+
+     // If someone sends a message, broadcast it to other people
+     socket.on('send', message=>{
+         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+     });
+
+     // If someone leaves the chat, let others know ('disconnect' is predefined event) -> left event is defined by us
+     socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+ })
